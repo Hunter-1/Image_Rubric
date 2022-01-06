b314e6435c51a8dca9b04b66a5c92a6976f9a675
Hunter <hunterinswitzerland@gmail.com>
2022-01-06 17:24:39 +0100

    Sub Buttons now appear at X,Y Coords described in Xml File

--- display.php
+++ display.php
@@ -10,7 +10,39 @@ $filepath = $dirname.$currentFile;
 $currentTitle = $currentImage->attributes()->title;
 $currentDescription = $currentImage->attributes()->description;
 $xml = simplexml_load_file("paths.xml");
-echo '<img alt="'.$currentTitle.'"src="'.$filepath.'" /><br />';
+echo '<head><title>Image Rubrics</title><style>
+#image_container>img {
+    position: relative;
+}';
+$i = 0;
+foreach ($currentImage->children() as $subImage){
+    $subTitle = $subImage->attributes()->title;
+    $subFile = $subImage->attributes()->file;
+    $subFile = str_replace(".", "\.",$subFile);
+    $subX = $subImage->attributes()->x;
+    $subY = $subImage->attributes()->y;
+    echo '
+#image_container>form>#'.$subFile.' {
+position: absolute;
+left: '.$subX.'px;
+top: '.$subY.'px;
+}';
+    $i++;
+}
+echo'</style></head>';
+echo '<div id="image_container">';
+echo '<img onclick="imageOnClick(this)" alt="'.$currentTitle.'"src="'.$filepath.'" /><br />';
+$i = 0;
+foreach ($currentImage->children() as $subImage){
+    $subTitle = $subImage->attributes()->title;
+    $subFile = $subImage->attributes()->file;
+    echo '<form method="get" action="display.php">
+    <input type="hidden" value="'.$subFile.'" name="file" id="file">
+    <button type="submit" id="'.$subFile.'">'.$subTitle.'</button>
+    </form>';
+    $i++;
+}
+echo '</div>';
 echo '<h1>'.$currentTitle.'</h1>';
 echo '<p>'.$currentDescription.'</p>';
 
@@ -24,15 +56,6 @@ if($currentImage != $xml->xpath("/images/image")){
     <button type="submit">'.$upperTitle.'</button>
     </form>';
     }
-}
-
-foreach ($currentImage->children() as $subImage){
-    $subTitle = $subImage->attributes()->title;
-    $subFile = $subImage->attributes()->file;
-    echo '<form method="get" action="display.php">
-    <input type="hidden" value="'.$subFile.'" name="file" id="file">
-    <button type="submit">'.$subTitle.'</button>
-    </form>';
 }
 
 echo '<form action="upload.php" method="post" enctype="multipart/form-data">
@@ -49,3 +72,33 @@ echo '<form action="upload.php" method="post" enctype="multipart/form-data">
     <input type="hidden" value="'.$currentFile.'" name="image" id="image">
     <input type="submit" value="Upload" name="submit">
 </form>';
+?>
+<script type="text/javascript">
+    function imageOnClick()
+    {
+        getEventLocation(event);
+    }
+    function getEventLocation(eventRef)
+    {
+        var positionX = 0;
+        var positionY = 0;
+
+        if ( eventRef.pageX )
+        {
+            positionX = eventRef.pageX;
+            positionY = eventRef.pageY;
+        }
+        else if ( window.event )
+        {
+            positionX = eventRef.clientX + document.body.scrollLeft;
+            positionY = eventRef.clientY + document.body.scrollTop;
+        }
+        else if ( eventRef.clientX )
+        {
+            positionX = eventRef.clientX;
+            positionY = eventRef.clientY;
+        }
+        document.getElementById("x").value = positionX;
+        document.getElementById("y").value = positionY;
+    }
+</script>
